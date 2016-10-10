3fafb484ea3103625643fa449bbc90d57a48f796
Anthony Green <anthony@atgreen.org>
2016-10-10 09:54:55 -0400

    fix typo

--- prepare-NSS-db.sh
+++ prepare-NSS-db.sh
@@ -6,4 +6,4 @@ head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | fold -w 8 | head -n 1
 certutil -d /etc/ipa/nssdb/ -N -f /etc/ipa/nssdb/pwdfile.txt
 certutil -A -d /etc/ipa/nssdb -n 'IPA CA' -t CT,C,C -a -i /root/ca.crt
 chmod 0644 /etc/ipa/nssdb/*.db
-chmod 0600 /etc/ipa-nssdb/pwdfile.txt
+chmod 0600 /etc/ipa/nssdb/pwdfile.txt
